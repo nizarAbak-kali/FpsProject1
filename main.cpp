b4bc447ab3ba153f95040153460904e7cc3cde54
nizar <nizarabakkali93@gmail.com>
2015-01-23 13:17:51 +0100

    creation du projet ajout sur github

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: nizar
+ *
+ * Created on 23 janvier 2015, 12:19
+ */
+
+
+/*
+ PROJET SDL /OPENGL SOUS NETBEANS 
+ * 
+ Pour compiler avec la SDL dans NetBeans Il suffit d'ajouter l'option 
+ * -lSDL dans les propriétés du projet, C compiler (ou C++) et dans 
+ * les options additionnelles.
+ *  On ajoute la directive preprocesseur :
+ *  #include <SDL/SDL.h>
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <SDL/SDL.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  
+
+    return 0;
+}
+
